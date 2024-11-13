5e8fb80e47a6f93be79611e69e26924dc253f38c
Mksignature <emas08177@gmail.com>
2024-11-13 13:26:48 +0100

    update 1.1.0

--- script/shop/fbase.js
+++ script/shop/fbase.js
@@ -8,69 +8,6 @@ let successIcon = `<dotlottie-player src="./images/success.json" background="tra
 
 let errorIcon = `<dotlottie-player src="./images/error.json" background="transparent" speed="1" style="width: auto; height: 300px;" loop autoplay></dotlottie-player>`;
 
-if (!loggedShopUser) {
-  let timeInterval = setTimeout(() => {
-    html = `
-        <div class="fixed-div">
-        <div
-          class="fade show special-modal match-content"
-          tabindex="-1"
-          role="dialog"
-          style="padding-right: 17px; display: block"
-        >
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button
-                  type="button"
-                  class="close js-close-btn"
-                  aria-label="Close"
-                >
-                  <span class="ti-close" aria-hidden="true"></span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <div class="row no-gutters">
-                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="notification-slide">
-                      <div class="notification-img">
-                        ${errorIcon}
-                      </div>
-                      <div class="notification-content">
-                        <h3>Not a customer yet?</h3>
-                        <p>Sign in to continue shopping exclusive content</p>
-                        <button
-                          class="btn"
-                          onclick="
-                          let loggedShopUser = localStorage.getItem('loggedShopUser')
-
-                          if (loggedShopUser) {
-                          window.location.href='./user/user.html'
-                          } else {
-                          window.location.href='./user/index.html'
-                          }"
-                        >
-                          Continue
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-    modalHTML.classList.remove("hidden");
-    modalHTML.innerHTML = html;
-    document.querySelector(".js-close-btn").addEventListener("click", () => {
-      modalHTML.classList.add("hidden");
-    });
-
-    clearTimeout(timeInterval);
-  }, 10000);
-}
 
 newsletterForm.addEventListener("submit", (e) => {
   e.preventDefault();
